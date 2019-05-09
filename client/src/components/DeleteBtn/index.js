baf42e602bb0641b7e717929796e069ffe9f0339
Kidus <Ketemakidus009@gmail.com>
2019-05-08 22:46:22 -0400

    fixing bug

--- client/src/components/DeleteBtn/index.js
+++ client/src/components/DeleteBtn/index.js
@@ -6,7 +6,7 @@ import "./style.css";
 function DeleteBtn(props) {
   return (
     <span className="delete-btn" {...props} role="button" tabIndex="0">
-      ✗
+    <button className="button button3">Delete</button>
     </span>
   );
 }
